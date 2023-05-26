92688218021ebfa74ce1ad78fb1ba8806de0d140
Okky Maheswara <okkymahes@gmail.com>
2023-05-26 12:52:20 +0800

    update

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-      Build by Okky Maheswara.
-      All right reserved.
+      Build by Okky Maheswara. All right reserved.
     </footer>
   )
 }
